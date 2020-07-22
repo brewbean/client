3664211810331ad112929a8bfa157360cd8f9343
jaamz <jamestkd918@gmail.com>
2020-07-22 03:14:15 -0700

    started coffee details component (just the shell)

--- coffee-frontend/src/components/BrewTrak/BrewTrakComment.js
+++ coffee-frontend/src/components/BrewTrak/BrewTrakComment.js
@@ -2,15 +2,12 @@ import React, { useState } from 'react';
 
 
 const BrewTrakComment = () => {
-    
+
     const [comment, setComment] = useState("Comment here...");
-    return(
-        <div>   
-            <div>
-                <textarea value={comment} onChange={(e) => setComment(e.target.value)} />
-               
-                
-            </div>
+    return (
+        <div>
+            <textarea value={comment} onChange={(e) => setComment(e.target.value)} />
+
         </div>
     )
 }
