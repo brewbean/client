fae679fa3f48501519f7e6aa11b5d5a27142aa33
Patrick Truong <pattruong@gmail.com>
2020-07-29 15:45:14 -0700

    UserProvider example

--- coffee-frontend/src/App.js
+++ coffee-frontend/src/App.js
@@ -1,14 +1,30 @@
 import React from 'react';
-import {Switch, Route} from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import BrewTrakHome from './components/BrewTrak/BrewTrakHome';
+import { UserProvider, useUser } from './context/userContext';
+import Header from './components/Header';
 
-
+function TestPage() {
+  const { user } = useUser();
+  return (
+    <Header />
+  )
+}
 
 function App() {
   return (
-    <Switch>
-      <Route exact path='/' component={BrewTrakHome} />
-    </Switch>
+    <>
+      {/* NON-USER EXPERIENCE */}
+      <Switch>
+        <Route exact path='/' component={BrewTrakHome} />
+      </Switch>
+      {/* USER EXPERIENCE */}
+      <UserProvider>
+        <Switch>
+          <Route path='/test' component={TestPage} />
+        </Switch>
+      </UserProvider>
+    </>
   );
 }
 
