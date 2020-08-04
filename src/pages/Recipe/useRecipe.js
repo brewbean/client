e598a3df017c650c158f554aee34a8ca1f01b76e
Patrick Truong <pattruong@gmail.com>
2020-08-04 16:31:48 -0700

    useRecipe hook

---
+++ src/pages/Recipe/useRecipe.js
@@ -0,0 +1,73 @@
+import { useState } from 'react';
+
+const initStage = () => ({
+  name: '',
+  weight: 0,
+  startTime: { min: 0, sec: 0 },
+  endTime: { min: 0, sec: 0 },
+})
+
+export const useRecipe = () => {
+  let [stages, setStages] = useState([initStage()])
+  let [serveTime, setServe] = useState({ min: 0, sec: 0 });
+
+  const addStage = () => {
+    setStages([
+      ...stages,
+      initStage()
+    ])
+  }
+
+  const setStageValue = index => ({ target }) => {
+    setStages([
+      ...stages.slice(0, index),
+      {
+        ...stages[index],
+        [target.name]: target.value
+      },
+      ...stages.slice(index + 1),
+    ])
+  }
+
+  const setTime = (key, index) => ({ target }) => {
+    setStages([
+      ...stages.slice(0, index),
+      {
+        ...stages[index],
+        [key]: {
+          ...stages[index][key],
+          [target.name]: target.value
+        }
+      },
+      ...stages.slice(index + 1),
+    ])
+  }
+
+  const deleteStage = index => () => {
+    setStages([
+      ...stages.slice(0, index),
+      ...stages.slice(index + 1),
+    ])
+  }
+
+  const setServeTime = ({ target }) => {
+    setServe({
+      ...serveTime,
+      [target.name]: target.value
+    })
+  }
+
+  return {
+    data: {
+      stages,
+      serveTime,
+    },
+    handler: {
+      addStage,
+      setTime,
+      deleteStage,
+      setStageValue,
+      setServeTime,
+    }
+  }
+}
