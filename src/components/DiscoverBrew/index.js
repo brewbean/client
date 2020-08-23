d6d8065f274c31bdba28bded0357f0d44d34c8d5
William Yang <williamyang93@gmail.com>
2020-08-22 22:34:50 -0700

    Renamed DiscoverDetail to DiscoverCard. Created sample data and passed in props for DiscoverCard

--- src/components/DiscoverBrew/index.js
+++ src/components/DiscoverBrew/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
-
+import DiscoverCard from './DiscoverCard';
+import {sampleDetails} from './sampleDetails';
 const DiscoverBrew = () => {
+    // let beanDetail = {name:"KENYA KAMWANGI", company_name:"Stereoscope", roast_type: "light", region: "Kirinyaga", profile_note: ["Red Grapefruit", "Cranberry"]}
+    let beanDetail = sampleDetails;
+    console.log("Beandetail", beanDetail);
     return (
         <div>
         <div className="bg-gray-800 pb-32">
@@ -17,42 +21,10 @@ const DiscoverBrew = () => {
             <div className="max-w-7xl mx-auto pb-12 px-4 sm:px-6 lg:px-8">
             {/* <!-- Replace with your content --> */}
                 <ul className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-                    <li className="col-span-1 flex flex-col text-center bg-white rounded-lg shadow">
-                        <div className="flex-1 flex flex-col p-8">
-                        <img className="w-32 h-32 flex-shrink-0 mx-auto bg-black rounded-full" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=4&amp;w=256&amp;h=256&amp;q=60" alt=""/>
-                        <h3 className="mt-6 text-gray-900 text-sm leading-5 font-medium">Jane Cooper</h3>
-                        <dl className="mt-1 flex-grow flex flex-col justify-between">
-                            <dt className="sr-only">Title</dt>
-                            <dd className="text-gray-500 text-sm leading-5">Paradigm Representative</dd>
-                            <dt className="sr-only">Role</dt>
-                            <dd className="mt-3">
-                            <span className="px-2 py-1 text-teal-800 text-xs leading-4 font-medium bg-teal-100 rounded-full">Admin</span>
-                            </dd>
-                        </dl>
-                        </div>
-                        <div className="border-t border-gray-200">
-                        <div className="-mt-px flex">
-                            <div className="w-0 flex-1 flex border-r border-gray-200">
-                            <div className="relative -mr-px w-0 flex-1 inline-flex items-center justify-center py-4 text-sm leading-5 text-gray-700 font-medium border border-transparent rounded-bl-lg hover:text-gray-500 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 transition ease-in-out duration-150">
-                                <svg className="w-5 h-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
-                                <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
-                                <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
-                                </svg>
-                                <span className="ml-3">Email</span>
-                            </div>
-                            </div>
-                            <div className="-ml-px w-0 flex-1 flex">
-                            <div className="relative w-0 flex-1 inline-flex items-center justify-center py-4 text-sm leading-5 text-gray-700 font-medium border border-transparent rounded-br-lg hover:text-gray-500 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 transition ease-in-out duration-150">
-                                <svg className="w-5 h-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
-                                <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
-                                </svg>
-                                <span className="ml-3">Call</span>
-                            </div>
-                            </div>
-                        </div>
-                        </div>
-                    </li>
-
+                    {beanDetail.map((x,i) =>
+                        <DiscoverCard  key={i} {...x}/>
+                    )}
+                    {/* <DiscoverCard {...beanDetail}/>
                     <li className="col-span-1 flex flex-col text-center bg-white rounded-lg shadow">
                         <div className="flex-1 flex flex-col p-8">
                         <img className="w-32 h-32 flex-shrink-0 mx-auto bg-black rounded-full" src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=4&amp;w=256&amp;h=256&amp;q=60" alt=""/>
@@ -159,151 +131,8 @@ const DiscoverBrew = () => {
                             </div>
                         </div>
                         </div>
-                    </li>
+                    </li> */}
 
-                    <li className="col-span-1 flex flex-col text-center bg-white rounded-lg shadow">
-                        <div className="flex-1 flex flex-col p-8">
-                        <img className="w-32 h-32 flex-shrink-0 mx-auto bg-black rounded-full" src="https://images.unsplash.com/photo-1532417344469-368f9ae6d187?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=4&amp;w=256&amp;h=256&amp;q=60" alt=""/>
-                        <h3 className="mt-6 text-gray-900 text-sm leading-5 font-medium">Kristin Watson</h3>
-                        <dl className="mt-1 flex-grow flex flex-col justify-between">
-                            <dt className="sr-only">Title</dt>
-                            <dd className="text-gray-500 text-sm leading-5">Investor Data Orchestrator</dd>
-                            <dt className="sr-only">Role</dt>
-                            <dd className="mt-3">
-                            <span className="px-2 py-1 text-teal-800 text-xs leading-4 font-medium bg-teal-100 rounded-full">Admin</span>
-                            </dd>
-                        </dl>
-                        </div>
-                        <div className="border-t border-gray-200">
-                        <div className="-mt-px flex">
-                            <div className="w-0 flex-1 flex border-r border-gray-200">
-                            <div className="relative -mr-px w-0 flex-1 inline-flex items-center justify-center py-4 text-sm leading-5 text-gray-700 font-medium border border-transparent rounded-bl-lg hover:text-gray-500 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 transition ease-in-out duration-150">
-                                <svg className="w-5 h-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
-                                <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
-                                <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
-                                </svg>
-                                <span className="ml-3">Email</span>
-                            </div>
-                            </div>
-                            <div className="-ml-px w-0 flex-1 flex">
-                            <div className="relative w-0 flex-1 inline-flex items-center justify-center py-4 text-sm leading-5 text-gray-700 font-medium border border-transparent rounded-br-lg hover:text-gray-500 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 transition ease-in-out duration-150">
-                                <svg className="w-5 h-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
-                                <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
-                                </svg>
-                                <span className="ml-3">Call</span>
-                            </div>
-                            </div>
-                        </div>
-                        </div>
-                    </li>
-
-                    <li className="col-span-1 flex flex-col text-center bg-white rounded-lg shadow">
-                        <div className="flex-1 flex flex-col p-8">
-                        <img className="w-32 h-32 flex-shrink-0 mx-auto bg-black rounded-full" src="https://images.unsplash.com/photo-1566492031773-4f4e44671857?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=4&amp;w=256&amp;h=256&amp;q=60" alt=""/>
-                        <h3 className="mt-6 text-gray-900 text-sm leading-5 font-medium">Cameron Williamson</h3>
-                        <dl className="mt-1 flex-grow flex flex-col justify-between">
-                            <dt className="sr-only">Title</dt>
-                            <dd className="text-gray-500 text-sm leading-5">Product Infrastructure Executive</dd>
-                            <dt className="sr-only">Role</dt>
-                            <dd className="mt-3">
-                            <span className="px-2 py-1 text-teal-800 text-xs leading-4 font-medium bg-teal-100 rounded-full">Admin</span>
-                            </dd>
-                        </dl>
-                        </div>
-                        <div className="border-t border-gray-200">
-                        <div className="-mt-px flex">
-                            <div className="w-0 flex-1 flex border-r border-gray-200">
-                            <div className="relative -mr-px w-0 flex-1 inline-flex items-center justify-center py-4 text-sm leading-5 text-gray-700 font-medium border border-transparent rounded-bl-lg hover:text-gray-500 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 transition ease-in-out duration-150">
-                                <svg className="w-5 h-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
-                                <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
-                                <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
-                                </svg>
-                                <span className="ml-3">Email</span>
-                            </div>
-                            </div>
-                            <div className="-ml-px w-0 flex-1 flex">
-                            <div className="relative w-0 flex-1 inline-flex items-center justify-center py-4 text-sm leading-5 text-gray-700 font-medium border border-transparent rounded-br-lg hover:text-gray-500 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 transition ease-in-out duration-150">
-                                <svg className="w-5 h-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
-                                <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
-                                </svg>
-                                <span className="ml-3">Call</span>
-                            </div>
-                            </div>
-                        </div>
-                        </div>
-                    </li>
-
-                    <li className="col-span-1 flex flex-col text-center bg-white rounded-lg shadow">
-                        <div className="flex-1 flex flex-col p-8">
-                        <img className="w-32 h-32 flex-shrink-0 mx-auto bg-black rounded-full" src="https://images.unsplash.com/photo-1534751516642-a1af1ef26a56?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=4&amp;w=256&amp;h=256&amp;q=60" alt=""/>
-                        <h3 className="mt-6 text-gray-900 text-sm leading-5 font-medium">Courtney Henry</h3>
-                        <dl className="mt-1 flex-grow flex flex-col justify-between">
-                            <dt className="sr-only">Title</dt>
-                            <dd className="text-gray-500 text-sm leading-5">Investor Factors Associate</dd>
-                            <dt className="sr-only">Role</dt>
-                            <dd className="mt-3">
-                            <span className="px-2 py-1 text-teal-800 text-xs leading-4 font-medium bg-teal-100 rounded-full">Admin</span>
-                            </dd>
-                        </dl>
-                        </div>
-                        <div className="border-t border-gray-200">
-                        <div className="-mt-px flex">
-                            <div className="w-0 flex-1 flex border-r border-gray-200">
-                            <div className="relative -mr-px w-0 flex-1 inline-flex items-center justify-center py-4 text-sm leading-5 text-gray-700 font-medium border border-transparent rounded-bl-lg hover:text-gray-500 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 transition ease-in-out duration-150">
-                                <svg className="w-5 h-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
-                                <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
-                                <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
-                                </svg>
-                                <span className="ml-3">Email</span>
-                            </div>
-                            </div>
-                            <div className="-ml-px w-0 flex-1 flex">
-                            <div className="relative w-0 flex-1 inline-flex items-center justify-center py-4 text-sm leading-5 text-gray-700 font-medium border border-transparent rounded-br-lg hover:text-gray-500 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 transition ease-in-out duration-150">
-                                <svg className="w-5 h-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
-                                <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
-                                </svg>
-                                <span className="ml-3">Call</span>
-                            </div>
-                            </div>
-                        </div>
-                        </div>
-                    </li>
-
-                    <li className="col-span-1 flex flex-col text-center bg-white rounded-lg shadow">
-                        <div className="flex-1 flex flex-col p-8">
-                        <img className="w-32 h-32 flex-shrink-0 mx-auto bg-black rounded-full" src="https://images.unsplash.com/photo-1488426862026-3ee34a7d66df?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=4&amp;w=256&amp;h=256&amp;q=60" alt=""/>
-                        <h3 className="mt-6 text-gray-900 text-sm leading-5 font-medium">Theresa Webb</h3>
-                        <dl className="mt-1 flex-grow flex flex-col justify-between">
-                            <dt className="sr-only">Title</dt>
-                            <dd className="text-gray-500 text-sm leading-5">Global Division Officer</dd>
-                            <dt className="sr-only">Role</dt>
-                            <dd className="mt-3">
-                            <span className="px-2 py-1 text-teal-800 text-xs leading-4 font-medium bg-teal-100 rounded-full">Admin</span>
-                            </dd>
-                        </dl>
-                        </div>
-                        <div className="border-t border-gray-200">
-                        <div className="-mt-px flex">
-                            <div className="w-0 flex-1 flex border-r border-gray-200">
-                            <div className="relative -mr-px w-0 flex-1 inline-flex items-center justify-center py-4 text-sm leading-5 text-gray-700 font-medium border border-transparent rounded-bl-lg hover:text-gray-500 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 transition ease-in-out duration-150">
-                                <svg className="w-5 h-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
-                                <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
-                                <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
-                                </svg>
-                                <span className="ml-3">Email</span>
-                            </div>
-                            </div>
-                            <div className="-ml-px w-0 flex-1 flex">
-                            <div className="relative w-0 flex-1 inline-flex items-center justify-center py-4 text-sm leading-5 text-gray-700 font-medium border border-transparent rounded-br-lg hover:text-gray-500 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 transition ease-in-out duration-150">
-                                <svg className="w-5 h-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
-                                <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
-                                </svg>
-                                <span className="ml-3">Call</span>
-                            </div>
-                            </div>
-                        </div>
-                        </div>
-                    </li>
                 </ul>
             {/* <!-- /End replace --> */}
             </div>
