0df13fc40d2109c8da1a299bc4841d42f86ecc12
Patrick Truong <pattruong@gmail.com>
2020-10-05 09:49:33 -0700

    Change API endpoint

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export const AUTH_API = process.env.NODE_ENV !== 'production'
-  ? 'http://localhost:4000'
-  : 'https://brewbean-api.herokuapp.com';
+  ? 'http://localhost:4000/auth'
+  : 'https://brewbean-api.herokuapp.com/auth';
 
 export const GRAPHQL_API = 'https://brewbean-graphql.herokuapp.com/v1/graphql';
 
