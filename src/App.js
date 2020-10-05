45bbbe445f06acfb7e9730291b5af9e2cd0b5959
Patrick Truong <pattruong@gmail.com>
2020-10-05 09:49:19 -0700

    Change route and user context order

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { createClient, Provider } from 'urql';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import { createClient, Provider, defaultExchanges } from 'urql';
+import { devtoolsExchange } from '@urql/devtools';
 
 import { GRAPHQL_API } from './config'
-import { UserProvider, useUser } from './context/userContext';
+import { useUser } from './context/userContext';
 
 import PourGuide from './pages/PourGuide';
 import BrewTrakPage from './pages/BrewTrak';
 import DiscoverBeanPage from './pages/DiscoverBean';
 import Recipe from './pages/Recipe';
-import BaristaRoutes from './BaristaRoutes';
+import Login from './pages/Login';
 
 function App() {
-  const { token } = useUser();
+  const { token, isAuthenticated } = useUser();
 
   const client = createClient({
     url: GRAPHQL_API,
+    exchanges: [devtoolsExchange, ...defaultExchanges],
     fetchOptions: () => ({
       headers: { authorization: `Bearer ${token}` },
     }),
@@ -23,21 +25,14 @@ function App() {
 
   return (
     <Provider value={client}>
-      <Router>
-        {/* NON-USER EXPERIENCE */}
-        <Switch>
-          <Route exact path='/' component={BrewTrakPage} />
-          <Route path='/pour-app' component={PourGuide} />
-          <Route path='/recipe' component={Recipe} />
-          <Route path='/brewtrak' component={BrewTrakPage} />
-          <Route path='/discover/bean' component={DiscoverBeanPage} />
-
-        </Switch>
-        {/* USER EXPERIENCE */}
-        <UserProvider>
-          <BaristaRoutes />
-        </UserProvider>
-      </Router>
+      <Switch>
+        <Route exact path='/' component={BrewTrakPage} />
+        <Route path='/login' render={props => isAuthenticated ? <Redirect {...props} to='/' /> : <Login {...props} />} />
+        <Route path='/pour-app' component={PourGuide} />
+        <Route path='/recipe' component={Recipe} />
+        <Route path='/brewtrak' component={BrewTrakPage} />
+        <Route path='/discover/bean' component={DiscoverBeanPage} />
+      </Switch>
     </Provider>
   );
 }
