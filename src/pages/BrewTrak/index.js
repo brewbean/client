de63dabcc5505befb32429a3cd43aae6bb519fc3
Patrick Truong <pattruong@gmail.com>
2020-10-14 22:43:21 -0700

    refactor nested route

--- src/pages/BrewTrak/index.js
+++ src/pages/BrewTrak/index.js
@@ -8,19 +8,18 @@ import CreateBrew from '../../components/BrewTrak/CreateBrew';
 
 const BrewTrakPage = props => {
   const { data, methods } = useBrewTrak();
-  let match = useRouteMatch();
-  
+  let { path } = useRouteMatch();
 
   return (
     <div className='h-screen bg-white flex flex-col'>
       <Header />
       <div className="bg-gray-50 flex-1 flex items-stretch">
         <div className='max-w-7xl bg-pink w-full mx-auto p-4 sm:px-6 lg:px-8'>
-        <Switch>
-          <Route exact path={`${match.url}/`} render={props => <BrewTrak {...props} {...data} {...methods}/> } /> 
-          <Route path={`${match.url}/new`} render={props => <CreateBrew {...props} {...data} {...methods} />} />
-        </Switch>
-          
+          <Switch>
+            <Route exact path={path} render={props => <BrewTrak {...props} {...data} {...methods} />} />
+            <Route path={`${path}/new`} render={props => <CreateBrew {...props} {...data} {...methods} />} />
+          </Switch>
+
         </div>
       </div>
     </div>
