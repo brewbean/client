3cfc8b6932b1e22564755c2d97bcff21e736bdc9
Patrick Truong <pattruong@gmail.com>
2020-11-09 16:40:06 -0800

    Remove Header component from individual pages

--- src/pages/BrewTrak/index.js
+++ src/pages/BrewTrak/index.js
@@ -1,25 +1,20 @@
 import { Switch, Route, useRouteMatch } from 'react-router-dom';
 
-import Header from 'components/Header';
 import BrewTrak from 'components/BrewTrak';
-import { useBrewTrak } from 'components/BrewTrak/useBrewTrak';
 import CreateBrew from 'components/BrewTrak/CreateBrew';
+import { useBrewTrak } from 'components/BrewTrak/useBrewTrak';
 
 const BrewTrakPage = props => {
   const { data, methods } = useBrewTrak();
   let { path } = useRouteMatch();
 
   return (
-    <div className='h-screen bg-white flex flex-col'>
-      <Header />
-      <div className="bg-gray-50 flex-1 flex items-stretch">
-        <div className='max-w-7xl bg-pink w-full mx-auto p-4 sm:px-6 lg:px-8'>
-          <Switch>
-            <Route exact path={path} render={props => <BrewTrak {...props} {...data} {...methods} />} />
-            <Route path={`${path}/new`} render={props => <CreateBrew {...props} {...data} {...methods} />} />
-          </Switch>
-
-        </div>
+    <div className="bg-gray-50 flex-1 flex items-stretch">
+      <div className='max-w-7xl bg-pink w-full mx-auto p-4 sm:px-6 lg:px-8'>
+        <Switch>
+          <Route exact path={path} render={props => <BrewTrak {...props} {...data} {...methods} />} />
+          <Route path={`${path}/new`} render={props => <CreateBrew {...props} {...data} {...methods} />} />
+        </Switch>
       </div>
     </div>
   )
--- src/pages/DiscoverBean/index.js
+++ src/pages/DiscoverBean/index.js
@@ -1,6 +1,5 @@
 import { Switch, Route, useRouteMatch } from 'react-router-dom';
 
-import Header from 'components/Header';
 import DiscoverBean from 'components/DiscoverBean';
 import DiscoverDetails from 'components/DiscoverBean/DiscoverDetails'
 import CreateReview from 'components/DiscoverBean/CreateReview'
@@ -11,18 +10,14 @@ const DiscoverBeanPage = props => {
   const { data, methods } = useDiscoverBean();
   let match = useRouteMatch();
   return (
-    <div className='h-screen bg-white flex flex-col'>
-      <Header />
-      <div className="bg-gray-50 flex-1 flex items-stretch">
-        <div className='max-w-7xl bg-pink w-full mx-auto p-4 sm:px-6 lg:px-8'>
+    <div className="bg-gray-50 flex-1 flex items-stretch">
+      <div className='max-w-7xl bg-pink w-full mx-auto p-4 sm:px-6 lg:px-8'>
         <Switch>
-          <Route exact path={`${match.url}/`} render={props => <DiscoverBean {...props} {...data} {...methods}/> } /> 
-          <Route path = {`${match.url}/details/:id`} render={props => <DiscoverDetails {...props}/> } />
-          <Route path = {`${match.url}/review/:id/new`} render={props => <CreateReview {...props}/> } />
+          <Route exact path={`${match.url}/`} render={props => <DiscoverBean {...props} {...data} {...methods} />} />
+          <Route path={`${match.url}/details/:id`} render={props => <DiscoverDetails {...props} />} />
+          <Route path={`${match.url}/review/:id/new`} render={props => <CreateReview {...props} />} />
           {/* <Route path={`${match.url}/new`} render={props => <CreateBean {...props} {...data} {...methods} />} /> */}
         </Switch>
-          
-        </div>
       </div>
     </div>
   )
