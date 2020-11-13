0ccca7c49f093acfd439b6d8d0129e9b1f3a636d
Patrick Truong <pattruong@gmail.com>
2020-11-13 14:53:53 -0800

    remove container styling

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -14,7 +14,7 @@ const Login = (props) => {
   }
 
   return (
-    <div className="min-h-screen bg-white flex">
+    <>
       <div className="flex-1 flex flex-col justify-center py-12 px-4 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
         <div className="mx-auto w-full max-w-sm lg:w-96">
 
@@ -70,7 +70,7 @@ const Login = (props) => {
       <div className="hidden lg:block relative w-0 flex-1">
         <img className="absolute inset-0 h-full w-full object-cover" src={coffeeCover} alt="espresso" />
       </div>
-    </div>
+    </>
 
   )
 }
