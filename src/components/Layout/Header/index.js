5f0684cb298b83b0f6d1e291e6139c580fa476b2
Patrick Truong <pattruong@gmail.com>
2020-11-18 15:36:05 -0800

    close dropdown when clicking off

--- src/components/Layout/Header/index.js
+++ src/components/Layout/Header/index.js
@@ -1,6 +1,7 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { useRouteMatch, Link } from 'react-router-dom';
 import { Transition } from '@headlessui/react'
+import { useUser } from 'context/UserContext';
 import UserSection from './UserSection';
 
 const links = [
@@ -33,16 +34,39 @@ const settingLinks = [
   },
 ];
 
+const PlaceholderAvatar = () => (
+  <span className="inline-block h-8 w-8 rounded-full overflow-hidden bg-blue-100">
+    <svg className="h-full w-full text-blue-300" fill="currentColor" viewBox="0 0 24 24">
+      <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
+    </svg>
+  </span>
+);
+
 const Header = () => {
   const { path } = useRouteMatch();
   const [isOpen, setToggle] = useState(false);
   const [isDropdownOpen, setDropdownOpen] = useState(false);
+  const { barista } = useUser();
+  const dropdownRef = useRef(null);
 
   useEffect(() => {
     // closes mobile menu/dropdown whenever a link is clicked
     setToggle(false);
     setDropdownOpen(false);
   }, [path]);
+  
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setDropdownOpen(false);
+      }
+    }
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      // Unbind the event listener on clean up
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [dropdownRef]);
 
   return (
     <nav className="flex-none bg-white border-b border-gray-200">
@@ -61,8 +85,12 @@ const Header = () => {
           <div className="hidden sm:ml-6 sm:flex sm:items-center">
             <div className="ml-3 relative">
               <div>
-                <button onClick={() => setDropdownOpen(!isDropdownOpen)} className="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition duration-150 ease-in-out" id="user-menu" aria-label="User menu" aria-haspopup="true">
-                  <img className="h-8 w-8 rounded-full" src="https://ca.slack-edge.com/TSCG4PBLN-USDKSPWF3-cdc2a3b92383-512" alt="" />
+                <button onClick={() => setDropdownOpen(!isDropdownOpen)} className="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-blue-300 transition duration-150 ease-in-out" id="user-menu" aria-label="User menu" aria-haspopup="true">
+                  {
+                    barista.avatar
+                      ? <img className="h-8 w-8 rounded-full" src={barista.avatar} alt="user avatar" />
+                      : <PlaceholderAvatar />
+                  }
                 </button>
               </div>
 
@@ -75,7 +103,7 @@ const Header = () => {
                 leaveFrom="transform opacity-100 scale-100"
                 leaveTo="transform opacity-0 scale-95"
               >
-                <div className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg">
+                <div className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg" ref={dropdownRef}>
                   <div className="py-1 rounded-md bg-white shadow-xs" role="menu" aria-orientation="vertical" aria-labelledby="user-menu">
                     {settingLinks.map(({ to, text }) => <Link key={to} to={to} className="block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out" role="menuitem">{text}</Link>)}
                     <button onClick={() => console.log('signout')} className="w-full text-left px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out" role="menuitem">sign out</button>
@@ -105,7 +133,7 @@ const Header = () => {
           {links.map((link, i) => <Link key={link.to} to={link.to} className={`menu__link ${path === link.to ? 'menu__link--state-active' : 'menu__link--state-inactive'} ${i > 0 ? 'mt-1' : ''}`.trimEnd()}>{link.text}</Link>)}
         </div>
         <div className="p-4 border-t border-gray-200">
-          <UserSection links={settingLinks}/>
+          <UserSection links={settingLinks} />
         </div>
       </div>
     </nav>
