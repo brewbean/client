2b0ef63ac5167a0354c701471bd278d87389d1ab
Patrick Truong <pattruong@gmail.com>
2020-11-19 13:56:52 -0800

    move container styling and alert to container

--- src/components/Layout/Container.js
+++ src/components/Layout/Container.js
@@ -1,10 +1,25 @@
 import Header from './Header';
+import Alert from 'components/Alert';
 
-const Container = ({ flexCol = true, header = true, children }) => {
+const Container = ({ flexCol = true, header = true, paddedContent = true, alert = true, children }) => {
   return (
-    <div className={`min-h-screen bg-white flex ${flexCol ? 'flex-col' : ''}`.trim()}>
+    <div className={`min-h-screen bg-gray-50 flex ${flexCol ? 'flex-col' : ''}`.trim()}>
       {header && <Header />}
-      {children}
+      {
+        paddedContent
+          ? (
+            <div className="flex-1 w-full max-w-7xl mx-auto p-4 sm:px-6 lg:px-8">
+              {alert && <Alert containerStyle='mb-4 space-y-2' />}
+              {children}
+            </div>
+          )
+          : (
+            <>
+              {alert && <Alert containerStyle='mb-4 space-y-2' />}
+              {children}
+            </>
+          )
+      }
     </div>
   )
 }
