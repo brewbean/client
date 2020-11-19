fedd10082a66df96b13196e01f58aeb0210611c2
Patrick Truong <pattruong@gmail.com>
2020-11-18 19:10:08 -0800

    refactor login/create  section into own components

--- src/components/Layout/Header/UserSection.js
+++ src/components/Layout/Header/UserSection.js
@@ -1,39 +1,79 @@
+import { useEffect, useRef } from 'react';
+import { Transition } from '@headlessui/react';
 import { Link } from 'react-router-dom';
 import { useUser } from 'context/UserContext';
 import PlaceholderAvatar from './PlaceholderAvatar';
 
-const UserSection = ({ links }) => {
-  const { isAuthenticated, barista } = useUser();
+const UserSection = ({ links, setDropdownOpen, isDropdownOpen }) => {
+  const dropdownRef = useRef(null);
+  const avatarRef = useRef(null);
+
+  const { isAuthenticated, isPending, needRefresh, barista } = useUser();
+
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (avatarRef.current && avatarRef.current.contains(event.target)) {
+        setDropdownOpen(!isDropdownOpen);
+        return;
+      }
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setDropdownOpen(false);
+      }
+    }
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      // Unbind the event listener on clean up
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [dropdownRef, avatarRef, isDropdownOpen, setDropdownOpen]);
+
+  // add pending image
+  if (isPending || needRefresh) return null;
 
   return isAuthenticated ? (
-    <>
-      <div className="flex items-center">
-        <div className="flex-shrink-0">
-          {
-            barista.avatar
-              ? <img className="h-10 w-10 rounded-full" src={barista.avatar} alt="user avatar" />
-              : <PlaceholderAvatar containerStyle='h-10 w-10' />
-          }
-        </div>
-        <div className="ml-3">
-          <div className="text-base font-medium leading-6 text-gray-800">{barista.displayName}</div>
-          <div className="text-sm font-medium leading-5 text-gray-500">{barista.email}</div>
-        </div>
+    <div className="hidden sm:ml-6 sm:flex sm:items-center">
+      <div>
+        <p className="text-sm tracking-wider font-medium text-gray-800">{barista.displayName}</p>
       </div>
-      <div className="mt-4 flex flex-col justify-start space-y-1" role="menu" aria-orientation="vertical" aria-labelledby="user-menu">
-        {links.map(({ to, text }) => <Link key={to} to={to} className="py-2 px-3 rounded-md text-left text-base font-medium text-gray-500 focus:outline-none focus:text-gray-800 focus:bg-gray-100" role="menuitem">{text}</Link>)}
-        <button onClick={() => console.log('signout')} className="py-2 px-3 rounded-md text-left text-base font-medium text-gray-500 focus:outline-none focus:text-gray-800 focus:bg-gray-100" role="menuitem">sign out</button>
+      <div className="ml-3 relative">
+        <div>
+          <button ref={avatarRef} className="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-50 transition duration-150 ease-in-out" id="user-menu" aria-label="User menu" aria-haspopup="true">
+            {
+              barista.avatar
+                ? <img className="h-8 w-8 rounded-full" src={barista.avatar} alt="user avatar" />
+                : <PlaceholderAvatar />
+            }
+          </button>
+        </div>
+
+        <Transition
+          show={isDropdownOpen}
+          enter="transition ease-out duration-200"
+          enterFrom="transform opacity-0 scale-95"
+          enterTo="transform opacity-100 scale-100"
+          leave="transition ease-in duration-75"
+          leaveFrom="transform opacity-100 scale-100"
+          leaveTo="transform opacity-0 scale-95"
+        >
+          <div className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5" ref={dropdownRef}>
+            <div className="py-1 rounded-md bg-white shadow-xs" role="menu" aria-orientation="vertical" aria-labelledby="user-menu">
+              {links.map(({ to, text }) => <Link key={to} to={to} className="block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out" role="menuitem">{text}</Link>)}
+              <button onClick={() => console.log('signout')} className="w-full text-left px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out" role="menuitem">log out</button>
+            </div>
+          </div>
+        </Transition>
+
       </div>
-    </>
+    </div>
   ) : (
-      <>
-        <Link to='/create-account' className="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-blue-600 focus:bg-blue-700">
-          create account
-        </Link>
-        <Link to='/login' className="mt-2 py-2 w-full flex items-center justify-center text-base font-medium text-blue-600 focus:text-blue-500">
+      <div className='hidden sm:flex sm:items-center'>
+        <Link to='/login' className="mr-3 inline-flex items-center px-3 py-2 border border-gray-300 shadow-sm text-sm leading-4 font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
           log in
         </Link>
-      </>
+        <Link to='/create-account' className="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md shadow-sm text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+          create account
+        </Link>
+      </div>
     );
 }
 
--- src/components/Layout/Header/index.js
+++ src/components/Layout/Header/index.js
@@ -1,9 +1,7 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect } from 'react';
 import { useRouteMatch, Link } from 'react-router-dom';
-import { Transition } from '@headlessui/react'
-import { useUser } from 'context/UserContext';
 import UserSection from './UserSection';
-import PlaceholderAvatar from './PlaceholderAvatar';
+import UserSectionMobile from './UserSectionMobile';
 
 const links = [
   {
@@ -39,32 +37,12 @@ const Header = () => {
   const { path } = useRouteMatch();
   const [isOpen, setToggle] = useState(false);
   const [isDropdownOpen, setDropdownOpen] = useState(false);
-  const { isAuthenticated, barista } = useUser();
-  const dropdownRef = useRef(null);
-  const avatarRef = useRef(null);
-
+  
   useEffect(() => {
     // closes mobile menu/dropdown whenever a link is clicked
     setToggle(false);
     setDropdownOpen(false);
   }, [path]);
-
-  useEffect(() => {
-    function handleClickOutside(event) {
-      if (avatarRef.current && avatarRef.current.contains(event.target)) {
-        setDropdownOpen(!isDropdownOpen);
-        return;
-      }
-      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
-        setDropdownOpen(false);
-      }
-    }
-    document.addEventListener("mousedown", handleClickOutside);
-    return () => {
-      // Unbind the event listener on clean up
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
-  }, [dropdownRef, avatarRef, isDropdownOpen]);
 
   return (
     <nav className="flex-none bg-white border-b border-gray-200">
@@ -72,7 +50,7 @@ const Header = () => {
         <div className="flex justify-between h-16">
           <div className="flex">
             <div className="flex-shrink-0 flex items-center">
-              <Link to='/' className="ml-2 font-extrabold tracking-widest text-blue-500">brew<span className='text-pink-400'>(</span>bean<span className='text-pink-400'>)</span></Link>
+              <Link to='/' className="ml-2 text-lg font-extrabold tracking-widest text-blue-500">brew<span className='text-pink-400'>(</span>bean<span className='text-pink-400'>)</span></Link>
             </div>
             <div className="hidden sm:-my-px sm:ml-6 sm:flex">
               {
@@ -81,51 +59,11 @@ const Header = () => {
             </div>
           </div>
 
-          {isAuthenticated ? (
-            <div className="hidden sm:ml-6 sm:flex sm:items-center">
-              <div>
-                <p className="text-sm font-medium text-gray-500">{barista.displayName}</p>
-              </div>
-              <div className="ml-3 relative">
-                <div>
-                  <button ref={avatarRef} className="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:ring-1 focus:ring-blue-400 focus:ring-opacity-50 transition duration-150 ease-in-out" id="user-menu" aria-label="User menu" aria-haspopup="true">
-                    {
-                      barista.avatar
-                        ? <img className="h-8 w-8 rounded-full" src={barista.avatar} alt="user avatar" />
-                        : <PlaceholderAvatar />
-                    }
-                  </button>
-                </div>
-
-                <Transition
-                  show={isDropdownOpen}
-                  enter="transition ease-out duration-200"
-                  enterFrom="transform opacity-0 scale-95"
-                  enterTo="transform opacity-100 scale-100"
-                  leave="transition ease-in duration-75"
-                  leaveFrom="transform opacity-100 scale-100"
-                  leaveTo="transform opacity-0 scale-95"
-                >
-                  <div className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5" ref={dropdownRef}>
-                    <div className="py-1 rounded-md bg-white shadow-xs" role="menu" aria-orientation="vertical" aria-labelledby="user-menu">
-                      {settingLinks.map(({ to, text }) => <Link key={to} to={to} className="block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out" role="menuitem">{text}</Link>)}
-                      <button onClick={() => console.log('signout')} className="w-full text-left px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out" role="menuitem">sign out</button>
-                    </div>
-                  </div>
-                </Transition>
-
-              </div>
-            </div>
-          ) : (
-              <div className='hidden sm:flex sm:items-center'>
-                <Link to='/login' className="mr-3 inline-flex items-center px-3 py-2 border border-gray-300 shadow-sm text-sm leading-4 font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
-                  log in
-                </Link>
-                <Link to='/create-account' className="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md shadow-sm text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
-                  create account
-                </Link>
-              </div>
-            )}
+          <UserSection
+            links={settingLinks}
+            isDropdownOpen={isDropdownOpen}
+            setDropdownOpen={setDropdownOpen}
+          />
 
           <div className="-mr-2 flex items-center sm:hidden">
             <button onClick={() => setToggle(!isOpen)}
@@ -147,7 +85,7 @@ const Header = () => {
           {links.map((link, i) => <Link key={link.to} to={link.to} className={`menu__link ${path === link.to ? 'menu__link--state-active' : 'menu__link--state-inactive'} ${i > 0 ? 'mt-1' : ''}`.trimEnd()}>{link.text}</Link>)}
         </div>
         <div className="p-4 border-t border-gray-200">
-          <UserSection links={settingLinks} />
+          <UserSectionMobile links={settingLinks} />
         </div>
       </div>
     </nav>
