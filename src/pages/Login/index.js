38d2f5b636db5468885ef8d314e4ed7bf79d6331
Patrick Truong <pattruong@gmail.com>
2020-11-19 18:50:58 -0800

    disable login button on error

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -54,7 +54,7 @@ const Login = (props) => {
               </div>
               <div>
                 <span className="pt-2 block w-full rounded-md shadow-sm">
-                  <button type="submit" className="w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-500 focus:outline-none focus:border-blue-700 focus:shadow-outline-blue active:bg-blue-700 transition duration-150 ease-in-out">
+                  <button disabled={hasAlert} type="submit" className={`disabled:opacity-50 w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 focus:outline-none focus:border-blue-700 focus:shadow-outline-blue active:bg-blue-700 transition duration-150 ease-in-out ${!hasAlert ? 'hover:bg-blue-500' : 'cursor-not-allowed'}`}>
                     log in
                  </button>
                 </span>
