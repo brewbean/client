d550b66f67d5657b1c6a2c4f809171f5b9321f1c
Patrick Truong <pattruong@gmail.com>
2020-12-08 11:07:20 -0800

    Cleanup config prop for route styling - add footer option

--- src/components/Layout/Container.js
+++ src/components/Layout/Container.js
@@ -1,25 +1,38 @@
 import Header from './Header';
+import Footer from './Footer';
 import Alert from 'components/Alert';
 
-const Container = ({ flexCol = true, header = true, paddedContent = true, alert = true, children }) => {
+const Container = ({ noLayout = false, config, children }) => {
+  let settings = noLayout
+    ? { ...config }
+    : {
+      flexCol: true,
+      header: true,
+      footer: true,
+      paddedContent: true,
+      alert: true,
+      ...config
+    }
+  
   return (
-    <div className={`min-h-screen bg-gray-50 flex ${flexCol ? 'flex-col' : ''}`.trim()}>
-      {header && <Header />}
+    <div className={`min-h-screen bg-gray-50 flex ${settings.flexCol ? 'flex-col' : ''}`.trim()}>
+      {settings.header && <Header />}
       {
-        paddedContent
+        settings.paddedContent
           ? (
             <div className="flex-1 w-full max-w-7xl mx-auto p-4 sm:px-6 lg:px-8">
-              {alert && <Alert containerStyle='mb-4 space-y-2' />}
+              {settings.alert && <Alert containerStyle='mb-4 space-y-2' />}
               {children}
             </div>
           )
           : (
             <>
-              {alert && <Alert containerStyle='mb-4 space-y-2' />}
+              {settings.alert && <Alert containerStyle='mb-4 space-y-2' />}
               {children}
             </>
           )
       }
+      {settings.footer && <Footer />}
     </div>
   )
 }
