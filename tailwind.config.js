f3fbf947a5fd808ec404913b34cb05ab5341c332
Patrick Truong <pattruong@gmail.com>
2020-12-09 14:32:56 -0800

    add new screen sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,13 @@ module.exports = {
     }
   },
   theme: {
-    extend: {},
+    extend: {
+      height: {
+        'screen-1/4': '25vh',
+        'screen-1/2': '50vh',
+        'screen-3/4': '75vh',
+      }
+    },
   },
   variants: {
     extend: {
