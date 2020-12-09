a9d7c9d174134c56d0e4ca3ece1e906de4112e55
Patrick Truong <pattruong@gmail.com>
2020-12-09 15:24:37 -0800

    Add basic submit functionality - no redirect

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,19 @@
+import { useState } from 'react';
+
 import CoffeeCover from './hero_beans.jpg';
 import Beans from './beans.jpg';
 
 export default function Home() {
+  const [search, setSearch] = useState('');
+
+  const onChange = ({ target }) => setSearch(target.value);
+
+  const onSubmit = e => {
+    e.preventDefault();
+    console.log('search sent');
+    setSearch('');
+  }
+
   return (
     <div className='grid grid-cols-2 gap-4'>
 
@@ -16,15 +28,15 @@ export default function Home() {
 
 
 
-        <div className="mt-10 flex rounded-md shadow-lg w-full sm:w-2/3 lg:w-1/2 xl:w-1/3">
-          <input type="text" className="focus:ring-blue-500 focus:border-blue-500 border-none block w-full rounded-none rounded-l-md sm:text-sm" placeholder="Colombian" />
+        <form onSubmit={onSubmit} className="mt-10 flex rounded-md shadow-lg w-full sm:w-2/3 lg:w-1/2 xl:w-1/3">
+          <input type="text" value={search} onChange={onChange} className="focus:ring-blue-500 focus:border-blue-500 border-none block w-full rounded-none rounded-l-md sm:text-sm" placeholder="Colombian" />
 
-          <button className="-ml-px relative inline-flex items-center px-4 py-2 text-sm font-medium rounded-r-md bg-blue-400 hover:bg-blue-500 focus:outline-none focus:ring-1 focus:ring-blue-500 focus:border-blue-500">
+          <button type='submit' className="-ml-px relative inline-flex items-center px-4 py-2 text-sm font-medium rounded-r-md bg-blue-400 hover:bg-blue-500 focus:outline-none focus:ring-1 focus:ring-blue-500 focus:border-blue-500">
             <svg className="w-6 h-6 text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
               <path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd" />
             </svg>
           </button>
-        </div>
+        </form>
 
 
 
