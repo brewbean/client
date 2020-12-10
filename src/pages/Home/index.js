120f288cb586b5e172f6e452c1a6c7f654869dce
Patrick Truong <pattruong@gmail.com>
2020-12-09 16:39:00 -0800

    Change hero text styling

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -21,15 +21,12 @@ export default function Home() {
     <>
       <div className='h-screen-3/4 bg-cover px-8 py-6 flex flex-col items-center justify-center' style={{ backgroundImage: `url(${CoffeeCover})` }}>
 
-
-        <div>
-          <h1 className='text-5xl tracking-tight font-extralight text-white'>Discover the world of coffee</h1>
-          <h2 className='text-xl mt-3 font-semibold text-white'>Explore and share coffee recipes, reviews, and more</h2>
+        <div className='text-center space-y-2'>
+          <h1 className='text-4xl tracking-tight font-extrabold sm:text-5xl md:text-6xl text-white'>Discover the world of coffee</h1>
+          <h2 className='sm:text-lg md:text-xl text-gray-100'>Explore and share coffee recipes, reviews, and more</h2>
         </div>
 
-
-
-        <form onSubmit={onSubmit} className="mt-10 flex rounded-md shadow-lg w-full sm:w-2/3 lg:w-1/2 xl:w-1/3">
+        <form onSubmit={onSubmit} className="mt-6 flex rounded-md shadow-lg w-full sm:w-2/3 lg:w-1/2 xl:w-1/3">
           <input type="text" value={search} onChange={onChange} className="focus:ring-blue-500 focus:border-blue-500 border-none block w-full rounded-none rounded-l-md sm:text-sm" placeholder="Colombian" />
 
           <button type='submit' className="-ml-px relative inline-flex items-center px-4 py-2 text-sm font-medium rounded-r-md bg-blue-400 hover:bg-blue-500 focus:outline-none focus:ring-1 focus:ring-blue-500 focus:border-blue-500">
