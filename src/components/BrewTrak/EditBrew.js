86dff8a4e0bc6c7c822c9137a442de35b6ea71a2
William Yang <williamyang93@gmail.com>
2020-12-12 16:05:55 -0800

    Ran format on EditBrew (not official prettify). Renamed Edit to EditBrewForm.

--- src/components/BrewTrak/EditBrew.js
+++ src/components/BrewTrak/EditBrew.js
@@ -1,26 +1,27 @@
-import { useState, useEffect } from 'react';
-import { useBrewTrak } from 'components/BrewTrak/useBrewTrak';
-import { GET_SINGLE_RECIPE, UPDATE_RECIPE, GET_SINGLE_BEAN_ID_BY_NAME } from 'queries';
-import { useQuery, useMutation } from 'urql';
-import { useParams } from 'react-router-dom';
-import Edit from './Edit';
+import { useState, useEffect } from "react";
+import { useBrewTrak } from "components/BrewTrak/useBrewTrak";
+import {
+  GET_SINGLE_RECIPE,
+  UPDATE_RECIPE,
+  GET_SINGLE_BEAN_ID_BY_NAME
+} from "queries";
+import { useQuery, useMutation } from "urql";
+import { useParams } from "react-router-dom";
+import EditBrewForm from "./EditBrewForm";
 
 const EditBrew = () => {
-    const { id } = useParams()
-    const [result, reexecuteQuery] = useQuery({
-        query: GET_SINGLE_RECIPE,
-        variables: { id }
-    });
-    const { data, fetching, error } = result;
-
-    if (fetching) return <p>Loading...</p>;
-    if (error) return <p>Oh no... {error.message}</p>;
-    if (data?.recipe_by_pk) {
-        return (
-            <Edit id={id} recipe={data.recipe_by_pk}/>
-        )
-    }
-    return null;
-}
+  const { id } = useParams();
+  const [result, reexecuteQuery] = useQuery({
+    query: GET_SINGLE_RECIPE,
+    variables: { id }
+  });
+  const { data, fetching, error } = result;
+  if (fetching) return <p>Loading...</p>;
+  if (error) return <p>Oh no... {error.message}</p>;
+  if (data?.recipe_by_pk) {
+    return <EditBrewForm id={id} recipe={data.recipe_by_pk} />;
+  }
+  return null;
+};
 
 export default EditBrew;
