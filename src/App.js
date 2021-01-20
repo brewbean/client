b737772f281f683d249e922d7da4cf91c6b137b6
Patrick Truong <pattruong@gmail.com>
2021-01-19 20:40:08 -0800

    add redirect based on trailing slash

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Switch } from 'react-router-dom'
+import { Switch, Redirect, useLocation } from 'react-router-dom'
 import { AuthRoute, RedirectRoute, ContainerRoute } from 'navigation'
 
 import { NotFound } from 'components/Utility'
@@ -19,8 +19,10 @@ const PathTest = () => {
 }
 
 function App() {
+  const { pathname, search } = useLocation()
   return (
     <Switch>
+      <Redirect from='/:url*(/+)' to={pathname.slice(0, -1) + search} />
       <ContainerRoute exact path='/' config={{ paddedContent: false }}>
         <Home />
         <NewUserModal />
