ab23b1478a82b0a16596926afe810afd79b43a6b
William Yang <williamyang93@gmail.com>
2021-01-22 22:39:52 -0800

    Edit Recipe implemented

--- src/components/Recipe/RecipeDetails.js
+++ src/components/Recipe/RecipeDetails.js
@@ -78,6 +78,12 @@ const RecipeDetails = (props) => {
                 >
                   submit review
                 </Link>
+                <Link
+                  to={`${url}/edit`}
+                  className='mb-4 inline-flex items-center px-4 py-2 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-blue-600 hover:bg-blue-500 focus:outline-none focus:border-blue-700 focus:shadow-outline-blue active:bg-blue-700 transition ease-in-out duration-150'
+                >
+                  EDIT RECIPE
+                </Link>
                 <button
                   onClick={deleteRecipePressed}
                   type='button'
--- src/pages/Recipe/index.js
+++ src/pages/Recipe/index.js
@@ -2,6 +2,7 @@ import { Switch, Route, useRouteMatch } from 'react-router-dom'
 import Recipes from 'components/Recipe'
 import RecipeDetails from 'components/Recipe/RecipeDetails'
 import CreateRecipe from 'components/Recipe/CreateRecipe'
+import EditRecipe from 'components/Recipe/EditRecipe'
 import CreateRecipeReview from 'components/Recipe/CreateRecipeReview'
 import EditRecipeReview from 'components/Recipe/EditRecipeReview'
 import StagePage from './StagePage'
@@ -17,7 +18,10 @@ const RecipePage = (props) => {
       <Route exact path={`${url}/new`}>
         <CreateRecipe /> {/*this needs to be CreateRecipe*/}
       </Route>
-      <Route path={`${url}/:id`}>
+      <Route path={`${url}/:id/edit`}>
+        <EditRecipe />
+      </Route>
+      <Route exact path={`${url}/:id`}>
         <RecipeDetails />
       </Route>
 
