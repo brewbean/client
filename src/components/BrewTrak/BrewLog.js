12508b08b2a3c73e088336c091f84f9aba835bd7
William Yang <williamyang93@gmail.com>
2021-01-28 23:38:49 -0800

    Sidebar set up with new design. Still has some comments left

--- src/components/BrewTrak/BrewLog.js
+++ src/components/BrewTrak/BrewLog.js
@@ -1,8 +1,36 @@
-import { firstInitials } from 'helper/stringHelper'
+import CoffeeCup from './Icons/coffee-cup.png'
 const BrewLog = ({ logs, setId, setBrewSelected }) => {
   return (
-    <div>
-      <div
+    <div
+      className='relative px-6 py-5 flex items-center space-x-3 hover:bg-gray-50 focus-within:ring-2 focus-within:ring-inset focus-within:ring-pink-500'
+      onClick={() => {
+        setId(logs.id)
+        setBrewSelected(true)
+      }}
+    >
+      <div className='flex-shrink-0'>
+        <img
+          className='h-10 w-10 rounded-full'
+          src={logs.bean ? logs.bean?.img : CoffeeCup}
+          alt=''
+        />
+      </div>
+      <div className='flex-1 min-w-0'>
+        <div className='focus:outline-none'>
+          {/* <!-- Extend touch target to entire panel --> */}
+          <span className='absolute inset-0' aria-hidden='true'></span>
+          <p className='text-sm font-medium text-gray-900'>
+            {logs.bean ? logs.bean?.name : 'No Bean Provided'}
+          </p>
+          <p className='text-sm text-gray-500 truncate'>{logs.date_added}</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default BrewLog
+/* <div
         className='col-span-1 flex shadow-sm rounded-md'
         onClick={() => {
           setId(logs.id)
@@ -17,35 +45,9 @@ const BrewLog = ({ logs, setId, setBrewSelected }) => {
             <div className='text-gray-900 font-medium hover:text-gray-600'>
               {logs.bean ? logs.bean?.name : 'No Bean Provided'}
             </div>
-            <p className='text-gray-500'>{logs.date_added}</p>
+            <p className='text-gray-500'></p>
           </div>
         </div>
       </div>
-    </div>
-    // return (
-    //   <div
-    //     classNameName='group flex flex-col px-4 py-2 text-sm leading-5 rounded-md font-medium text-gray-900 bg-gray-50 border border-gray-250 '
-    //     onClick={() => {
-    //       setId(logs.id)
-    //       setBrewSelected(true)
-    //     }}
-    //   >
-    //     <div classNameName='flex'>{logs.date_added}</div>
-    //     <div classNameName='flex'>
-    //       <div classNameName='flex-col'>
-    //         <div classNameName='flex pr-1'>Type: {logs.bean?.name}</div>
-    //       </div>
-    //       <div classNameName='flex-col'>
-    //         <div classNameName='flex px-1'>{logs.bean_weight}g</div>
-    //       </div>
-    //       <div classNameName='flex-col'>
-    //         <div classNameName='flex px-1'>{logs.water_temp}F</div>
-    //       </div>
-    //     </div>
-    //     <div classNameName='flex'>{logs.rating}/5</div>
-    //   </div>
-    // )
-  )
-}
-
-export default BrewLog
+     */
+// </div>
