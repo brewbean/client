9e5dea026a026515f4c9b89b5f2d1689c3891bcb
William Yang <williamyang93@gmail.com>
2021-01-29 23:03:15 -0800

    Started redesigning recipe page with tailwind UI

--- src/components/Recipe/index.js
+++ src/components/Recipe/index.js
@@ -24,29 +24,29 @@ const Recipes = () => {
   if (error) return <p>Oh no... {error.message}</p>
 
   return (
-    <div>
-      <div className='bg-gray-800 pb-32'>
-        <header className='py-10'>
-          <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
-            <h1 className='text-3xl leading-9 font-bold text-white'>Recipes</h1>
+    <div className='relative bg-gray-50 pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8'>
+      <div className='absolute inset-0'>
+        <div className='bg-white h-1/3 sm:h-2/3'></div>
+      </div>
+      <div className='relative max-w-7xl mx-auto'>
+        <div className='text-center'>
+          <h2 className='text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl'>
+            Recipes
+          </h2>
+          <div className='mt-3 max-w-2xl mx-auto text-xl text-gray-500 sm:mt-4'>
+            Explore coffee recipes here!
           </div>
           <Link
             to={`${url}/new`}
-            className='mb-4 inline-flex items-center px-4 py-2 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-blue-600 hover:bg-blue-500 focus:outline-none focus:border-blue-700 focus:shadow-outline-blue active:bg-blue-700 transition ease-in-out duration-150'
+            className='mb-4 mt-4 inline-flex items-center px-4 py-2 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-blue-600 hover:bg-blue-500 focus:outline-none focus:border-blue-700 focus:shadow-outline-blue active:bg-blue-700 transition ease-in-out duration-150'
           >
             new recipe
           </Link>
-        </header>
-      </div>
-      <main className='-mt-32'>
-        <div className='max-w-7xl mx-auto pb-12 px-4 sm:px-6 lg:px-8'>
-          {/* <!-- Replace with your content --> */}
-          <ul className='grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
-            {data && data.recipes.map((x, i) => <RecipeCard key={i} {...x} />)}
-          </ul>
-          {/* <!-- /End replace --> */}
         </div>
-      </main>
+        <div className='mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none'>
+          {data && data.recipes.map((x, i) => <RecipeCard key={i} {...x} />)}
+        </div>
+      </div>
     </div>
   )
 }
