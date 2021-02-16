8c690d16d36b874b9f43b0ae7f386ceccdea4634
Patrick Truong <pattruong@gmail.com>
2021-02-16 15:39:24 -0800

    implement tailwind CRA canonical setup - refactor purge setting - rename tailwindcss import

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter as Router } from 'react-router-dom'
 
 import App from 'App'
-import 'tailwind.generated.css'
+import './index.css'
 import { AlertProvider } from 'context/AlertContext'
 import { AuthProvider } from 'context/AuthContext'
 import { ModalProvider } from 'context/ModalContext'
