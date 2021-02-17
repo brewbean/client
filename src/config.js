5290160a60b986843226df90e06a69e821d35035
Patrick Truong <pattruong@gmail.com>
2021-02-16 20:45:18 -0800

    fix endpoint domain

--- src/config.js
+++ src/config.js
@@ -1,11 +1,11 @@
 export const AUTH_API =
   process.env.NODE_ENV !== 'production'
     ? 'http://localhost:4000/auth'
-    : 'https://brewbean-api.herokuapp.com/auth'
+    : 'https://api.brewbean.io/auth'
 
 export const VERIFY_API =
   process.env.NODE_ENV !== 'production'
     ? 'http://localhost:4000/verify'
-    : 'https://brewbean-api.herokuapp.com/verify'
+    : 'https://api.brewbean.io/verify'
 
 export const GRAPHQL_API = 'https://brewbean-graphql.herokuapp.com/v1/graphql'
