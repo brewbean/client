3fe61eab82434f8c318e15b97fd94dcacb44e8b9
William Yang <williamyang93@gmail.com>
2021-03-06 15:14:26 -0800

    Added props as extra options. Trying to add dynamic state but hitting re-renders?

--- src/components/Form/Row.js
+++ src/components/Form/Row.js
@@ -1,6 +1,57 @@
+import { useState } from 'react'
+import Dropdown from 'components/DropDown'
 import InputRow from 'components/InputRow'
+import TextArea from 'components/TextArea'
 
+const initState = {
+  form: {},
+}
 export const Row = ({ config }) => {
+  const [state, setState] = useState(initState)
+
+  const SwitchForm = ({
+    type,
+    name,
+    id,
+    placeholder,
+    label,
+    props,
+    options,
+  }) => {
+    console.log('name: ', name)
+    return (
+      <>
+        {type === 'input' && (
+          <InputRow
+            id={id}
+            value={state.name}
+            onChange={onChangeGenerator(name)}
+            placeholder={placeholder}
+            label={label}
+            {...props}
+            // required={required}
+          />
+        )}
+        {type === 'dropdown' && (
+          <Dropdown id={id} options={options} {...props} />
+        )}
+        {type === 'textarea' && <TextArea id={id} label={label} {...props} />}
+      </>
+    )
+  }
+
+  const onChangeGenerator = (attr) => ({ target }) => {
+    const { value, type } = target
+    console.log('Value', value)
+    console.log('Type', type)
+    console.log('target', target)
+
+    setState({
+      ...state,
+      [attr]: value,
+    })
+  }
+  console.log('rerendered')
   return (
     <div className='space-y-6 sm:space-y-5'>
       {config.map((c, i) => (
@@ -13,14 +64,7 @@ export const Row = ({ config }) => {
           </div>
           <div className='mt-4 sm:mt-0 sm:col-span-2 space-y-2'>
             {c.rows.map((r, i) => (
-              <InputRow
-                id={r.id}
-                // value={form.name}
-                // onChange={onChangeGenerator('name')}
-                placeholder={r.placeholder}
-                label={r.label}
-                required
-              />
+              <SwitchForm {...r} />
             ))}
           </div>
         </div>
--- src/pages/BrewLog/Create/index.js
+++ src/pages/BrewLog/Create/index.js
@@ -10,33 +10,56 @@ const Create = () => {
           id: 'recipe-name',
           placeholder: 'e.g. My favorite pour over recipe',
           label: 'Recipe Name',
+          name: 'name',
+          props: {
+            required: true,
+          },
         },
-        {
-          type: 'input',
-          id: 'recipe-about',
-          placeholder: 'e.g. Super amazing elixir',
-          label: 'About',
-        },
+        // {
+        //   type: 'input',
+        //   id: 'recipe-about',
+        //   placeholder: 'e.g. Super amazing elixir',
+        //   label: 'About',
+
+        // },
       ],
     },
-    {
-      rowTitle: 'Basics 2',
-      rowDescription: 'Tell us some details about your recipe 2',
-      rows: [
-        {
-          type: 'input',
-          id: 'recipe-name',
-          placeholder: 'e.g. My favorite pour over recipe!!',
-          label: 'Recipe Name',
-        },
-        {
-          type: 'input',
-          id: 'recipe-about',
-          placeholder: 'e.g. Super amazing elixir!!!',
-          label: 'About',
-        },
-      ],
-    },
+    // {
+    //   rowTitle: 'Basics 2',
+    //   rowDescription: 'Tell us some details about your recipe 2',
+    //   rows: [
+    //     {
+    //       type: 'input',
+    //       id: 'recipe-name',
+    //       placeholder: 'e.g. My favorite pour over recipe!!',
+    //       label: 'Recipe Name',
+
+    //     },
+    //     {
+    //       type: 'input',
+    //       id: 'recipe-about',
+    //       placeholder: 'e.g. Super amazing elixir!!!',
+    //       label: 'About',
+    //       props: {
+    //         required: true
+    //       }
+    //     },
+    //     {
+    //       type: 'dropdown',
+    //       id: 'bean-grind',
+    //       options: [
+    //         { key: 'Extra-fine', value: 'Extra-fine' },
+    //         { key: 'Fine', value: 'Fine' },
+    //         { key: 'Medium-fine', value: 'Medium-fine' },
+    //         { key: 'Medium-coarse', value: 'Medium-coarse' },
+    //         { key: 'Coarse', value: 'Coarse' },
+    //         { key: 'Extra-coarse', value: 'Extra-coarse' },
+    //       ],
+    //       label: 'Bean Grind'
+
+    //     }
+    //   ],
+    // },
   ]
   return (
     <div>
