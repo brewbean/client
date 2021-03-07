65e6ae0cb9f7835c22095457139c1da16c6595ce
William Yang <williamyang93@gmail.com>
2021-03-06 16:04:03 -0800

    Added in reducer. Removed SwitchForm due to re-rendering issues

--- src/components/Form/Row.js
+++ src/components/Form/Row.js
@@ -1,66 +1,48 @@
-// import { useState } from 'react'
+import { useReducer } from 'react'
 import Dropdown from 'components/DropDown'
 import InputRow from 'components/InputRow'
 import TextArea from 'components/TextArea'
 
-// const initState = {
-//   form: {},
-// }
+const initState = {
+  form: {},
+}
 
-// const reducer = (state, [type, payload]) => {
-//   switch (type) {
-//     case 'setForm':
-//       return {
-//         ...state,
-//         form: {
-//           ...state.form,
-//           [payload.key]: payload.value,
-//         },
-//       }
-//     default:
-//       return state
-//   }
-// }
-const SwitchForm = ({ type, name, id, placeholder, label, props, options }) => {
-  console.log('name: ', name)
+const reducer = (state, [type, payload]) => {
   switch (type) {
-    case 'input':
-      return (
-        <InputRow
-          id={id}
-          // value={state.name}
-          // onChange={onChangeGenerator(name)}
-          placeholder={placeholder}
-          label={label}
-          {...props}
-          // required={required}
-        />
-      )
-    case 'dropdown':
-      return <Dropdown id={id} options={options} {...props} />
-    case 'textarea':
-      return <TextArea id={id} label={label} {...props} />
+    case 'setForm':
+      return {
+        ...state,
+        form: {
+          ...state.form,
+          [payload.key]: payload.value,
+        },
+      }
     default:
-      return null
+      return state
   }
 }
-export const Row = ({ config }) => {
-  // const onChangeGenerator = (attr) => ({ target }) => {
-  //   const { value, type } = target
-  //   console.log('Value', value)
-  //   console.log('Type', type)
-  //   console.log('target', target)
 
-  //   setState({
-  //     ...state,
-  //     [attr]: value,
-  //   })
-  // }
-  console.log('rerendered')
+export const Row = ({ config }) => {
+  const [{ form }, dispatch] = useReducer(reducer, initState)
+
+  const onChangeGenerator = (attr) => ({ target }) => {
+    const { value } = target
+    dispatch([
+      'setForm',
+      {
+        key: attr,
+        value: value,
+      },
+    ])
+  }
+
   return (
     <div className='space-y-6 sm:space-y-5'>
       {config.map((c, i) => (
-        <div className='sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5'>
+        <div
+          key={i}
+          className='sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5'
+        >
           <div>
             <h4 className='text-md text-gray-900'>{c.rowTitle}</h4>
             <p className='mt-1 max-w-xs text-sm text-gray-500'>
@@ -69,7 +51,24 @@ export const Row = ({ config }) => {
           </div>
           <div className='mt-4 sm:mt-0 sm:col-span-2 space-y-2'>
             {c.rows.map((r, i) => (
-              <SwitchForm {...r} />
+              <div key={i}>
+                {r.type === 'input' && (
+                  <InputRow
+                    id={r.id}
+                    value={form[r.name] || ''}
+                    onChange={onChangeGenerator(r.name)}
+                    placeholder={r.placeholder}
+                    label={r.label}
+                    {...r.props}
+                  />
+                )}
+                {r.type === 'dropdown' && (
+                  <Dropdown id={r.id} options={r.options} {...r.props} />
+                )}
+                {r.type === 'textarea' && (
+                  <TextArea id={r.id} label={r.label} {...r.props} />
+                )}
+              </div>
             ))}
           </div>
         </div>
