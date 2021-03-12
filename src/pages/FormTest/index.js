7154aa5afbbd8ec64e519c52acd6e00a77e64056
William Yang <williamyang93@gmail.com>
2021-03-11 21:51:58 -0800

    commiting formtest

--- src/pages/FormTest/index.js
+++ src/pages/FormTest/index.js
@@ -1,88 +1,47 @@
-import { useState } from 'react'
+import { useForm } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as yup from 'yup'
 
-const FormErros = ({ errors }) => {
-  return (
-    <div>
-      {Object.keys(errors).map((fieldName, i) => {
-        if (errors[fieldName].length > 0) {
-          return (
-            <p key={i}>
-              {fieldName} {errors[fieldName]}
-            </p>
-          )
-        } else {
-          return null
-        }
-      })}
-    </div>
-  )
-}
+const schema = yup.object().shape({
+  firstName: yup.string().required(),
+  age: yup.number().positive().integer().required(),
+})
+
 const FormTest = () => {
-  const [state, setState] = useState({
-    email: '',
-    password: '',
-    errors: { email: '', password: '' },
-    emailValid: false,
-    passwordValid: false,
+  const { register, handleSubmit, watch, errors } = useForm({
+    resolver: yupResolver(schema),
   })
-  const validateField = (name, value) => {
-    let fieldValidationErrors = state.errors
-    let { emailValid, passwordValid } = state
-    console.log('Validating Field', name, value)
-    switch (name) {
-      case 'email':
-        emailValid = value.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i)
-        fieldValidationErrors.email = emailValid ? '' : ' is invalid'
-        break
-      case 'password':
-        passwordValid = value.length >= 6
-        console.log('Value: ', value)
-        console.log('PasswordValid: ', passwordValid)
-
-        fieldValidationErrors.password = passwordValid ? '' : ' is too short'
-        break
-      default:
-        break
-    }
-    setState({
-      ...state,
-      errors: fieldValidationErrors,
-      emailValid,
-      passwordValid,
-    })
-  }
-  const handleOnChange = ({ target }) => {
-    const { name, value } = target
-    validateField(name, value)
-    setState({
-      ...state,
-      [name]: value,
-    })
-  }
-
+  const onSubmit = (data) => console.log(data)
+  console.log('Error', errors)
+  console.log(watch('example')) // watch input value by passing the name of it
   return (
-    <div>
-      <h1>FormTest Page</h1>
-      <label>Email</label>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input type='text' name='firstName' ref={register} />
+      <p>{errors.firstName?.message}</p>
 
-      <input
-        className='appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5'
-        type='email'
-        name='email'
-        id='email'
-        value={state.email}
-        onChange={handleOnChange}
-      />
-      <input
-        className='appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5'
-        type='password'
-        name='password'
-        id='password'
-        value={state.password}
-        onChange={handleOnChange}
-      />
-      <FormErros errors={state.errors} />
-    </div>
+      <input type='text' name='age' ref={register} />
+      <p>{errors.age?.message}</p>
+
+      <input type='submit' />
+    </form>
   )
+  // return (
+  //   /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
+  //   <form onSubmit={handleSubmit(onSubmit)}>
+  //     {/* register your input into the hook by invoking the "register" function */}
+  //     <input name='example' defaultValue='test' ref={register} />
+  //     <label>Example Required</label>
+  //     {/* include validation with required or other standard HTML validation rules */}
+  //     <input name='exampleRequired' ref={register({ required: true })} />
+  //     {/* errors will return when field validation fails  */}
+  //     {errors.exampleRequired && <span>This field is required</span>}
+  //     <br/>
+  //     <label>Input Required</label>
+  //     <input name='inputRequired' ref={register({ required: true })} />
+  //     {/* errors will return when field validation fails  */}
+  //     {errors.exampleRequired && <span>This field is required</span>}
+  //     <input type='submit' />
+  //   </form>
+  // )
 }
 export default FormTest
