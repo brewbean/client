cf0dc3ea31086a19324882b1c5c344a92eb99f2f
William Yang <williamyang93@gmail.com>
2021-04-10 20:28:08 -0700

    Added fathom client and added FATHOM_ID to netlify toml

--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
 export const AUTH_API = process.env.REACT_APP_API_BASE + '/auth'
 export const VERIFY_API = process.env.REACT_APP_API_BASE + '/verify'
 export const GRAPHQL_API = process.env.REACT_APP_GRAPHQL_API
+export const FATHOM_ID = process.env.REACT_APP_FATHOM_ID
